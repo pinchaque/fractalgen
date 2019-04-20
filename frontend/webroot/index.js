cace29f0a65db1a8ee9a495630693d328b1b3610
Chuck Smith <chuck@pinchaque.com>
2019-04-20 11:18:34 -0700

    Wire up fractal bounds to input fields

--- frontend/webroot/index.js
+++ frontend/webroot/index.js
@@ -1,3 +1,4 @@
+
 var app = new Vue({
   el: '#app',
   data: {
@@ -6,6 +7,15 @@ var app = new Vue({
         xmax: 1.0,
         ymin: -1.0,
         ymax: 1.0
+    },
+  },
+  computed: {
+    fractal_url: function () {
+      return "http://localhost:8000/?xmin=" + this.form.xmin
+        + "&xmax=" + this.form.xmax
+        + "&ymin=" + this.form.ymin
+        + "&ymax=" + this.form.ymax
+        + "&width=500&height=500"
     }
   }
 })

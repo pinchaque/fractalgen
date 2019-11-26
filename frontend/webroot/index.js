d9b213f5274f689e455751f431c60aba737589fc
Chuck Smith <chuck@pinchaque.com>
2019-11-25 23:39:44 -0800

    Start work on click-to-zoom. Something's not quite right in the math but at least I have the mouse action wired up to something that will refresh the image.

--- frontend/webroot/index.js
+++ frontend/webroot/index.js
@@ -1,12 +1,14 @@
-
 var app = new Vue({
   el: '#app',
   data: {
+    img_width: 500,
+    img_height: 500,
+    zoom_factor: 2.0,
     form : {
-        xmin: -1.0,
+        xmin: -2.0,
         xmax: 1.0,
-        ymin: -1.0,
-        ymax: 1.0
+        ymin: -1.1,
+        ymax: 1.1
     },
   },
   computed: {
@@ -15,7 +17,37 @@ var app = new Vue({
         + "&xmax=" + this.form.xmax
         + "&ymin=" + this.form.ymin
         + "&ymax=" + this.form.ymax
-        + "&width=500&height=500"
+        + "&width=" + this.img_width
+        + "&height=" + this.img_height
+    }
+  },
+  methods: {
+    zoomIn: function (event) {
+
+      xdiff = this.form.xmax - this.form.xmin
+      xperc = (event.offsetX - 1.0) / this.img_width
+      xnew = this.form.xmin + (xperc * xdiff)
+
+      ydiff = this.form.ymax - this.form.ymin
+      yperc = 1.0 - ((event.offsetY - 1.0) / this.img_height)
+      ynew = this.form.ymin + (yperc * ydiff)
+
+      xdiff2 = xdiff / 2.0 / this.zoom_factor
+      this.form.xmin = xnew - xdiff2
+      this.form.xmax = xnew + xdiff2
+
+      ydiff2 = ydiff / 2.0 / this.zoom_factor
+      this.form.ymin = ynew - ydiff2
+      this.form.ymax = ynew + ydiff2
+
+      // `this` inside methods points to the Vue instance
+      // `event` is the native DOM event
+      alert('coord[' + event.offsetX + "," + event.offsetY + "]"
+        + ' / perc[' + xperc + "," + yperc + "]"
+        + ' / new[' + xnew + "," + ynew + "]"
+        + ' / x_range[' + this.form.xmin + "," + this.form.xmax + "]"
+        + ' / y_range[' + this.form.ymin + "," + this.form.ymax + "]"
+      )
     }
   }
 })

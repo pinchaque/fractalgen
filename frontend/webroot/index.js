355864ca943aadfa23be355d4e0638157e7db9d4
Chuck Smith <chuck@pinchaque.com>
2019-12-28 20:53:10 -0800

    Change so that the fractal is the full size of the window and resizes as the window is resized. The debug info is now an overlay. (#2)

--- frontend/webroot/index.js
+++ frontend/webroot/index.js
@@ -1,44 +1,70 @@
+// Returns a function, that, as long as it continues to be invoked, will not
+// be triggered. The function will be called after it stops being called for
+// N milliseconds. If `immediate` is passed, trigger the function on the
+// leading edge, instead of the trailing.
+function debounce(func, wait, immediate) {
+	var timeout;
+	return function() {
+		var context = this, args = arguments;
+		var later = function() {
+			timeout = null;
+			if (!immediate) func.apply(context, args);
+		};
+		var callNow = immediate && !timeout;
+		clearTimeout(timeout);
+		timeout = setTimeout(later, wait);
+		if (callNow) func.apply(context, args);
+	};
+};
+
 var app = new Vue({
   el: '#app',
   data: {
-    imgWidth: 500,
-    imgHeight: 500,
+    imgWidth: 100,
+    imgHeight: 100,
     zoomFactor: 2.0,
     cursorX: 0,
     cursorY: 0,
     coordX: 0,
     coordY: 0,
-    form : {
-        xMin: -2.0,
-        xMax: 1.0,
-        yMin: -1.1,
-        yMax: 1.1
-    },
+    xMin: -2.0,
+    xMax: 1.0,
+    yMin: -1.1,
+    yMax: 1.1
+  },
+  mounted: function () {
+    this.updateDimensions()
   },
   computed: {
     fractalUrl: function () {
-      return "http://localhost:8000/?xmin=" + this.form.xMin
-        + "&xmax=" + this.form.xMax
-        + "&ymin=" + this.form.yMin
-        + "&ymax=" + this.form.yMax
+      return "http://localhost:8000/?xmin=" + this.xMin
+        + "&xmax=" + this.xMax
+        + "&ymin=" + this.yMin
+        + "&ymax=" + this.yMax
         + "&width=" + this.imgWidth
         + "&height=" + this.imgHeight;
     }
   },
+  created() {
+    window.addEventListener("resize", this.updateDimensions);
+  },
+  destroyed() {
+    window.removeEventListener("resize", this.updateDimensions);
+  },
   methods: {
     xRange: function () {
-      return this.form.xMax - this.form.xMin;
+      return this.xMax - this.xMin;
     },
     yRange: function () {
-      return this.form.yMax - this.form.yMin;
+      return this.yMax - this.yMin;
     },
     pixelToCoordX: function (v) {
       var perc = (v - 1.0) / this.imgWidth;
-      return this.form.xMin + (perc * this.xRange());
+      return this.xMin + (perc * this.xRange());
     },
     pixelToCoordY: function (v) {
       var perc = 1.0 - ((v - 1.0) / this.imgHeight);
-      return this.form.yMin + (perc * this.yRange());
+      return this.yMin + (perc * this.yRange());
     },
     mouseMove: function (event) {
       this.cursorX = event.offsetX;
@@ -51,12 +77,16 @@ var app = new Vue({
       var yNew = this.pixelToCoordY(event.offsetY);
 
       var diffX = this.xRange() / 2.0 / this.zoomFactor;
-      this.form.xMin = xNew - diffX;
-      this.form.xMax = xNew + diffX;
+      this.xMin = xNew - diffX;
+      this.xMax = xNew + diffX;
 
       var diffY = this.yRange() / 2.0 / this.zoomFactor;
-      this.form.yMin = yNew - diffY;
-      this.form.yMax = yNew + diffY;
-    }
+      this.yMin = yNew - diffY;
+      this.yMax = yNew + diffY;
+    },
+    updateDimensions: debounce(function() {
+      this.imgWidth = window.innerWidth;
+      this.imgHeight = window.innerHeight;
+    }, 250)
   }
 })

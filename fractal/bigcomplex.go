0d3da406f9e6291c36badfbbcd1f4d68aa27a17a
Chuck Smith <chuck@pinchaque.com>
2019-12-29 22:59:59 -0800

    Enrich testing framework and add test for addition.

--- fractal/bigcomplex.go
+++ fractal/bigcomplex.go
@@ -8,6 +8,13 @@ type Complex struct {
 	Real, Imag big.Float
 }
 
+func NewComplex(real float64, imag float64) *Complex {
+  c := new(Complex)
+  c.Real = *big.NewFloat(real)
+  c.Imag = *big.NewFloat(imag)
+  return c
+}
+
 func (z *Complex) Add(x, y *Complex) *Complex {
   z.Real.Add(&x.Real, &y.Real)
   z.Imag.Add(&x.Imag, &y.Imag)
--- fractal/bigcomplex_test.go
+++ fractal/bigcomplex_test.go
@@ -1,31 +1,51 @@
 package fractal
 
 import (
-  "math/big"
   "testing"
+  "fmt"
 )
 
-func compare_float64(t *testing.T, exp float64, act float64, msg string) {
+func CompareFloat64(t *testing.T, exp float64, act float64, msg string) {
   if exp != act {
     t.Errorf("%s: expected %f got %f", msg, exp, act)
   }
 }
 
-func compare_complex(t *testing.T, exp complex128, act Complex, msg string) {
+func CompareComplex(t *testing.T, exp complex128, act *Complex, msg string) {
   act_r, _ := act.Real.Float64()
   act_i, _ := act.Imag.Float64()
 
   exp_r := real(exp)
   exp_i := imag(exp)
 
-  compare_float64(t, exp_r, act_r, msg + " real")
-  compare_float64(t, exp_i, act_i, msg + " imaginary")
+  CompareFloat64(t, exp_r, act_r, msg + " real")
+  CompareFloat64(t, exp_i, act_i, msg + " imaginary")
+}
+
+func GetData() []complex128 {
+  return []complex128 { 2.1+1.3i,
+    0+9.221i,
+    0-3.4591i,
+    1.3,
+    -9.008,
+    5.8823-4991.334i,
+    -3.9811+84.511i }
 }
 
 func TestIdentity(t *testing.T) {
-  r := 2.1
-  i := 1.3
-  c := Complex{ *big.NewFloat(r), *big.NewFloat(i) }
-  z := complex(r, i + 0.001)
-  compare_complex(t, z, c, "Basic identity")
+  for i, v := range GetData() {
+    t.Logf("Data[%d] = %f", i, v)
+    c := NewComplex(real(v), imag(v))
+    CompareComplex(t, v, c, "Basic identity")
+	}
+}
+
+func TestSum(t *testing.T) {
+  v1 := complex(5.8823, -4991.334)
+  v2 := complex(-3.9811, 84.511)
+  exp := v1 + v2
+  c1 := NewComplex(real(v1), imag(v1))
+  c2 := NewComplex(real(v2), imag(v2))
+  c1.Add(c1, c2)
+  CompareComplex(t, exp, c1, fmt.Sprintf("Adding %f and %f", v1, v2))
 }

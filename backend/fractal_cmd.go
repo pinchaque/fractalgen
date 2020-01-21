8f090df5eb113178127a52a4c0ebb09774276101
Chuck Smith <chuck@pinchaque.com>
2020-01-20 16:10:09 -0800

    Create command-line arguments for everything. For now this only supports float64 values on the CL, which is all we need for profiling.

--- backend/fractal_cmd.go
+++ backend/fractal_cmd.go
@@ -2,22 +2,18 @@ package main
 
 import (
   "github.com/pinchaque/fractalgen/fractal"
+  "flag"
   "log"
   "os"
   "math/big"
   "time"
   )
 
-//var params Params
-//var species = flag.String("xmin", "gopher", "the species we are studying")
-
-
 func main() {
-  prm := getParams()
+  prm, filename := getParams()
   start := time.Now()
 
   buffer := fractal.GenerateImage(prm)
-  filename := "image.png"
 
   f, _ := os.Create(filename)
   if _, err := f.Write(buffer.Bytes()); err != nil {
@@ -25,23 +21,35 @@ func main() {
   }
   f.Close()
   elapsed := time.Since(start)
-  log.Printf("(%s, %s) - (%s, %s) %dms",
+  log.Printf("(%s, %s) - (%s, %s) %dms -> %s",
     prm.XMin.String(),
     prm.YMin.String(),
     prm.XMax.String(),
     prm.YMax.String(),
-    elapsed.Milliseconds())
+    elapsed.Milliseconds(),
+    filename)
 }
 
-func getParams() fractal.Params {
+func getParams() (fractal.Params, string) {
+  filename := flag.String("o", "image.png", "Output filename (PNG)")
+  xmin := flag.Float64("xmin", -2.0, "X minimum")
+  xmax := flag.Float64("xmax", 1.5, "X maximum")
+  ymin :=flag.Float64("ymin", -1.5, "Y minimum")
+  ymax := flag.Float64("ymax", 1.5, "Y maximum")
+  width := flag.Int("width", 1024, "Image width")
+  height := flag.Int("height", 1024, "Image height")
+  iter := flag.Int("iter", 200, "Iterations for convergence")
+  escape := flag.Float64("escape", 2.0, "Escape value")
+  flag.Parse()
+
   var prm fractal.Params
-  prm.XMin = big.NewFloat(-2.0)
-  prm.XMax = big.NewFloat(1.5)
-  prm.YMin = big.NewFloat(-1.8)
-  prm.YMax = big.NewFloat(1.8)
-  prm.Width = 256
-  prm.Height = 256
-  prm.Iterations = uint8(200)
-  prm.Escape = big.NewFloat(2.0)
-  return prm
+  prm.XMin = big.NewFloat(*xmin)
+  prm.XMax = big.NewFloat(*xmax)
+  prm.YMin = big.NewFloat(*ymin)
+  prm.YMax = big.NewFloat(*ymax)
+  prm.Width = *width
+  prm.Height = *height
+  prm.Iterations = uint8(*iter)
+  prm.Escape = big.NewFloat(*escape)
+  return prm, *filename
 }

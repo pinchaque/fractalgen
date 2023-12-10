547d8c75ef32d420ebd9273bc0b705e43c0a5c82
Chuck Smith <chuck@pinchaque.com>
2023-12-10 18:22:34 -0500

    debugging

--- frontend/src/components/fractal.jsx
+++ frontend/src/components/fractal.jsx
@@ -12,8 +12,8 @@ export default function Fractal() {
   const [xMax, setXMax] = useState(2.0);
   const [iterations, setIterations] = useState(200);
   const [escape, setEscape] = useState(2.0);
-  const [width, setWidth] = useState(0);
-  const [height, setHeight] = useState(0);
+  const [imgWidth, setImgWidth] = useState(0);
+  const [imgHeight, setImgHeight] = useState(0);
 
   function recomputeCoords(oldW, oldH, newW, newH) {
     if (!((oldW > 0) && (oldH > 0) && (newW > 0) && (newH > 0))) {
@@ -49,20 +49,26 @@ export default function Fractal() {
   // If this is a resize (we have an existing width and height) then we
   // maintain the same aspect ratio.
   function handleWindowResize() {
-    console.log("X1 Updated fractal dims: " + width + ", " + height);
+    console.log("handleWindowResize()");
+    console.log("X1 Updated fractal dims: " + imgWidth + ", " + imgHeight);
     console.log("X1 xmin:" + xMin);
+    console.log("X1 imgWidth:" + imgWidth);
+    console.log("X1 imgHeight:" + imgHeight);
     // if we have existing dimensions
-    if ((width > 0) && (height > 0)) {
+    if ((imgWidth > 0) && (imgHeight > 0)) {
       recomputeCoords(
-        width, height, 
+        imgWidth, imgHeight, 
         ref.current.clientWidth, ref.current.clientHeight);
     }
-    setWidth(ref.current.clientWidth);
-    setHeight(ref.current.clientHeight);
+    setImgWidth(ref.current.clientWidth);
+    setImgHeight(ref.current.clientHeight);
+    console.log("X2 imgWidth:" + imgWidth);
+    console.log("X2 imgHeight:" + imgHeight);
   }
 
   // Handle initial layout
   useLayoutEffect(() => {
+    console.log("useLayoutEffect()");
     handleWindowResize();
   }, [numbers]);
 
@@ -70,8 +76,10 @@ export default function Fractal() {
   useEffect(() => {
     let timeout = false;
     let delay = 250;
+    console.log("useEffect()");
 
     function debouncedWindowResize() {
+      console.log("deboucedWindowResize()");
       clearTimeout(timeout);
       timeout = setTimeout(handleWindowResize, delay);
     }
@@ -86,12 +94,14 @@ export default function Fractal() {
   // URL to generate the fractal image
   function url() {
     const u = new URL("http://localhost:8000");
+    console.log("X3 imgWidth:" + imgWidth);
+    console.log("X3 imgHeight:" + imgHeight);
     u.searchParams.append("xmin", xMin);
     u.searchParams.append("xmax", xMax);
     u.searchParams.append("ymin", yMin);
     u.searchParams.append("ymax", yMax);
-    u.searchParams.append("width", width);
-    u.searchParams.append("height", height);
+    u.searchParams.append("width", imgWidth);
+    u.searchParams.append("height", imgHeight);
     u.searchParams.append("iterations", iterations);
     u.searchParams.append("escape", escape);
     console.log(u.href);

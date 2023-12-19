60cac968602a0ed4e6b437ad6cd9107aa13a92b0
Chuck Smith <chuck@pinchaque.com>
2023-12-19 15:53:46 -0500

    Helper classes - not tested yet

---
+++ frontend/src/classes/fractal.js
@@ -0,0 +1,25 @@
+
+class Fractal {
+  #min = new Point(-2.0, -2.0);
+  #max = new Point(2.0, 2.0);
+  #iterations = 200;
+  #escape = 2.0;
+
+  constructor() {
+  }
+
+  get min() { return this.#min; }
+  get max() { return this.#max; }
+  get iterations() { return this.#iterations; }
+  get escape() { return this.#escape; }
+
+  set min(x) { this.#min = x; }
+  set max(x) { this.#max = x; }
+  set iterations(x) { this.#iterations = x; }
+  set escape(x) { this.#escape = x; }
+
+  setBounds(min, max) {
+    this.#min = min;
+    this.#max = max;
+  }
+}
